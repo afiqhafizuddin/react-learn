5252148dae0992a067ebe5d4956ca5e4c8053b30
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-04-14 00:02:23 +0800

    udpate App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <div className="content">
+        <h1>App Component</h1>
+      </div>
     </div>
   );
 }
