2e80996a9b134727f2ef1bc725c22eaa99aee297
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-04-16 16:22:27 +0800

    edit list of blogs and add styles

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,24 @@
 import { useState } from "react";
 
 const Home = () => {
-  const [name, setName] = useState("mario"); // reactive
-  const [age, setAge] = useState(25);
-  const handleClick = () => {
-    setName("luigi");
-  };
-
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>
-        {name} is {age} years old
-      </p>
-      <button onClick={handleClick}>Click Me!</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
