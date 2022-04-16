0763930a3aadbe88caa868c130b3900b4a14e56c
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-04-16 12:40:31 +0800

    handle clicking event using onClick methods

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
 const Home = () => {
+  const handleClick = () => {
+    console.log("hello, ninjas");
+  };
+
+  const clickAgain = (name) => {
+    console.log(`hello ${name}`);
+  };
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me!</button>
+      <button onClick={() => clickAgain("afiq")}>Click Again</button>
     </div>
   );
 };
